a13ca12593ae1e1283826c2ab5dd014329faecdc
seang sengly <seangsengly24@gmail.com>
2026-06-10 22:11:09 +0700

    add new env

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBy4ZF47XJfYkP1DhOZVXUbSgGOz_M6yJU",
-  authDomain: "h-project-72ffe.firebaseapp.com",
-  projectId: "h-project-72ffe",
-  storageBucket: "h-project-72ffe.firebasestorage.app",
-  messagingSenderId: "475873471809",
-  appId: "1:475873471809:web:6b2720af6d646e6c09c5c0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
